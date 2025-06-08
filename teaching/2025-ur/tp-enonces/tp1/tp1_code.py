932f64179a7a8f8d3d63f67cf4b41893e8a49d77
Andre Schrottenloher <aschrott@ptb-2m7lvl3.irisa.fr>
2025-06-08 16:15:20 +0200

    rebase

---
+++ teaching/2025-ur/tp-enonces/tp1/tp1_code.py
@@ -0,0 +1,20 @@
+import hashlib
+from random import randrange, getrandbits
+sha2 = hashlib.sha256
+
+
+print(sha2(b"Hello world").hexdigest())
+print(sha2("Hello world".encode()).hexdigest())
+
+def sha2Trunc(x, N=32):
+    if type(x) is not int:
+        raise TypeError("Expected an int")
+    fullHash = sha2(str(x).encode()).hexdigest()
+    return int(fullHash[:N//4],16)
+
+print( sha2Trunc(100) )
+
+
+
+
+
