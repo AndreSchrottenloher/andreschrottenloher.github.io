7de2e1cdee82b06773c13ba390f502a63e54d62c
Andre Schrottenloher <aschrott@ptb-2m7lvl3.irisa.fr>
2026-01-12 15:29:36 +0100

    update

--- teaching/2026-ur/tps-enonces/tp5/tp5_code.py
+++ teaching/2026-ur/tps-enonces/tp5/tp5_code.py
@@ -1,4 +1,5 @@
-
+import random
+random.seed(0)
 
 def int_to_bits(i: int, width: int=8)-> list[int]:
     return list([int(c) for c in '{:0{width}b}'.format(i, width=width)])
@@ -38,8 +39,59 @@ P1 = [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
 # 25-bit LFSR
 P2 = [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,1,0,0,1]
 
-#print( LFSR_period(P1, [1] + [0]*16), 2**17-1 )
-#print( LFSR_period(P2, [1] + [0]*24), 2**25-1 ) # prend un peu de temps
 
+
+def css(s: list[int], N: int) -> list[int]:
+    s1, s2 =  s[:16] + [1], s[16:] + [1]
+    c = 0
+    res = []
+    #TODO
+    
+
+# test your code:
+#assert css([1]*40,4) == [254, 255, 142, 185]
+
+# ------------------ testing for question 2
+
+def generate_s_x_z():
+    # Generate a random key
+    s = [random.randrange(2) for _ in range(40)]
+
+    # Recompute the first sequence
+    s1 = s[:16] + [1]
+    x = []
+    for i in range(3):
+        s1, bits1 = LFSR_multiple_steps(P1, s1, 8)
+        x.append( bits_to_int(bits1) )
+    z = css(s,3)
+    return s,x,z
+
+def compute_initial_state_s2(x: list[int], z: list[int]) -> list[int]:
+    """
+    x: 3 bytes of output of the LFSR s1
+    z: 3 bytes of output of CSS
+    Return: the entire initial state s2.
+    """
+    #TODO
+    pass
+
+# test your code:
+#s,x,z = generate_s_x_z()
+#assert compute_initial_state_s2(x,z) == s[16:]
+
+
+
+def attack(z: list[int]) -> list[int]:
+    """
+    z: 5 bytes of the CSS output
+    Return: the entire initial state s1.
+    """
+    #TODO    
+    pass
+
+# test your code:
+#s = [random.randrange(2) for _ in range(40)]
+#z = css(s, 5)
+#assert attack(z) == s
 
 
